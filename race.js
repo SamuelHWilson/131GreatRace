36eff171d489e8bb5d35588f5c1d0c0a21a93ed2
WILSON <swilsondev@outlook.com>
2017-10-12 20:05:25 -0500

    Did stuff.

--- race.js
+++ race.js
@@ -1,19 +1,27 @@
 //Setting up global vars
-var contestantImages = ["img/chrome.png", "img/firefox.png", "img/exploder.png", "img/opera.png"];
+var contestantImages = ["img/chrome.png", "img/firefox.png", "img/exploder.png", "img/opera.png", "img/safari.png"];
+var trackPadding = 5;
 var contestants;
 
-//Setup
-function Setup() {
+//Setting up global events
+$(document).ready(function() {
+    SetupRacers();
+    SetupHover();
 
-}
+    window.setInterval(HoverAll, 200);
+});
 
 //Setup functions
 function SetupRacers() {
     var track = $("#track");
-    for (var image = 0; image < contestantImages.length; image++) {
-        newContestant = document.createElement("img");
-        newContestant.src = contestantImages[image];
-        newContestant.classList.add("contestant");
-        track.add(newContestant);
+    var cCount = contestantImages.length;
+    var cHeight = (100 - (trackPadding * (cCount + 1))) / cCount;
+
+    for (var cNum = 0; cNum < cCount; cNum++) {
+        newContestant = $(document.createElement("img"));
+        newContestant.attr("src", contestantImages[cNum]);
+        newContestant.toggleClass("contestant");
+        newContestant.css({"top": (cHeight * cNum + trackPadding * (cNum + 1)) + "%", "height": cHeight + "%"});
+        track[0].appendChild(newContestant[0]);
     }
 }
